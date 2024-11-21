8c05a2b15cce662c297bb9d4b0eab3d1ac8372cb
yassinevic <yassinevic@gmail.com>
2024-11-21 12:45:30 +0100

    Delete webapp/src/stores directory

--- webapp/src/stores/store.ts
+++
@@ -1,5 +0,0 @@
-import { writable } from 'svelte/store';
-import type {  SentenceResponse } from '../types/sentence';
-
-// A writable store to track the array
-export const sentencesStore = writable<SentenceResponse>();
